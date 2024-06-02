0b4db25c322c917ed46b02676a49d0ad960a2d15
Heloisa Felizardo <helocmp@yahoo.com.br>
2024-06-02 12:47:15 -0300

    Adicionando comentarios da LivrariaOnline

--- collection-java-api/src/main/java/map/Ordenacao/LivrariaOnline/LivrariaOnline.java
+++ collection-java-api/src/main/java/map/Ordenacao/LivrariaOnline/LivrariaOnline.java
@@ -3,35 +3,46 @@ package main.java.map.Ordenacao.LivrariaOnline;
 import java.util.*;
 
 public class LivrariaOnline {
+    // Mapa que armazena os livros com o link como chave e o objeto Livro como valor.
     Map<String, Livro> livros;
 
+    // Construtor que inicializa o mapa de livros.
     public LivrariaOnline() {
         this.livros = new HashMap<>();
     }
 
+    // Método para adicionar um livro ao mapa.
     public void adicionarLivro(String link, Livro livro) {
         livros.put(link, livro);
     }
 
+    // Método para remover um livro do mapa com base no título.
     public void removerLivro(String titulo) {
         List<String> chavesRemover = new ArrayList<>();
+        // Itera sobre as entradas do mapa e adiciona as chaves dos livros que têm o título especificado à lista.
         for (Map.Entry<String, Livro> entry : livros.entrySet()) {
             if (entry.getValue().getTitulo().equalsIgnoreCase(titulo)) {
                 chavesRemover.add(entry.getKey());
             }
         }
+        // Remove os livros do mapa usando as chaves coletadas.
         for (String chave : chavesRemover) {
             livros.remove(chave);
         }
     }
 
+    // Método para exibir os livros ordenados por preço.
     public Map<String, Livro> exibirLivrosOrdenadosPorPreco() {
+        // Cria uma lista das entradas do mapa para ordenação.
         List<Map.Entry<String, Livro>> livrosParaOrdenarPorPreco = new ArrayList<>(livros.entrySet());
 
+        // Ordena a lista de entradas usando o ComparatorPorPreco.
         livrosParaOrdenarPorPreco.sort(new ComparatorPorPreco());
 
+        // Cria um LinkedHashMap para preservar a ordem de inserção.
         Map<String, Livro> livrosOrdenadosPorPreco = new LinkedHashMap<>();
 
+        // Insere as entradas ordenadas no LinkedHashMap.
         for (Map.Entry<String, Livro> entry : livrosParaOrdenarPorPreco) {
             livrosOrdenadosPorPreco.put(entry.getKey(), entry.getValue());
         }
@@ -39,13 +50,18 @@ public class LivrariaOnline {
         return livrosOrdenadosPorPreco;
     }
 
+    // Método para exibir os livros ordenados por autor.
     public Map<String, Livro> exibirLivrosOrdenadosPorAutor() {
+        // Cria uma lista das entradas do mapa para ordenação.
         List<Map.Entry<String, Livro>> livrosParaOrdenarPorAutor = new ArrayList<>(livros.entrySet());
 
+        // Ordena a lista de entradas usando o ComparatorPorAutor.
         livrosParaOrdenarPorAutor.sort(new ComparatorPorAutor());
 
+        // Cria um LinkedHashMap para preservar a ordem de inserção.
         Map<String, Livro> livrosOrdenadosPorAutor = new LinkedHashMap<>();
 
+        // Insere as entradas ordenadas no LinkedHashMap.
         for (Map.Entry<String, Livro> entry : livrosParaOrdenarPorAutor) {
             livrosOrdenadosPorAutor.put(entry.getKey(), entry.getValue());
         }
@@ -53,8 +69,10 @@ public class LivrariaOnline {
         return livrosOrdenadosPorAutor;
     }
 
+    // Método para pesquisar livros por autor.
     public Map<String, Livro> pesquisarLivrosPorAutor(String autor) {
         Map<String, Livro> livrosPorAutor = new LinkedHashMap<>();
+        // Itera sobre as entradas do mapa e adiciona os livros que têm o autor especificado ao LinkedHashMap.
         for (Map.Entry<String, Livro> entry : livros.entrySet()) {
             Livro livro = entry.getValue();
             if (livro.getAutor().equals(autor)) {
@@ -64,11 +82,14 @@ public class LivrariaOnline {
         return livrosPorAutor;
     }
 
+    // Método para obter o livro mais caro.
     public List<Livro> obterLivroMaisCaro() {
         List<Livro> livrosMaisCaros = new ArrayList<>();
         double precoMaisAlto = Double.MIN_VALUE;
 
+        // Verifica se o mapa está vazio.
         if (!livros.isEmpty()) {
+            // Encontra o maior preço entre os livros.
             for (Livro livro : livros.values()) {
                 if (livro.getPreco() > precoMaisAlto) {
                     precoMaisAlto = livro.getPreco();
@@ -78,6 +99,7 @@ public class LivrariaOnline {
             throw new NoSuchElementException("A livraria está vazia!");
         }
 
+        // Adiciona os livros que têm o maior preço à lista.
         for (Map.Entry<String, Livro> entry : livros.entrySet()) {
             if (entry.getValue().getPreco() == precoMaisAlto) {
                 Livro livroComPrecoMaisAlto = livros.get(entry.getKey());
@@ -87,11 +109,14 @@ public class LivrariaOnline {
         return livrosMaisCaros;
     }
 
+    // Método para obter o livro mais barato.
     public List<Livro> obterLivroMaisBarato() {
         List<Livro> livrosMaisBaratos = new ArrayList<>();
         double precoMaisBaixo = Double.MAX_VALUE;
 
+        // Verifica se o mapa está vazio.
         if (!livros.isEmpty()) {
+            // Encontra o menor preço entre os livros.
             for (Livro livro : livros.values()) {
                 if (livro.getPreco() < precoMaisBaixo) {
                     precoMaisBaixo = livro.getPreco();
@@ -101,6 +126,7 @@ public class LivrariaOnline {
             throw new NoSuchElementException("A livraria está vazia!");
         }
 
+        // Adiciona os livros que têm o menor preço à lista.
         for (Map.Entry<String, Livro> entry : livros.entrySet()) {
             if (entry.getValue().getPreco() == precoMaisBaixo) {
                 Livro livroComPrecoMaisBaixo = livros.get(entry.getKey());
@@ -110,4 +136,3 @@ public class LivrariaOnline {
         return livrosMaisBaratos;
     }
 }
-
